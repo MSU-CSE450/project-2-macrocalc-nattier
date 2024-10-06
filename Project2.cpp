5441156917a955980cef9411917992eb7b4c3ff6
Gasperev <gasperev@msu.edu>
2024-10-05 23:06:13 -0400

    Update Project2.cpp
    Added 2 error functions, one for a custom line number and the other one you just pass the token to get the line number.

--- Project2.cpp
+++ Project2.cpp
@@ -3,13 +3,48 @@
 #include <iostream>
 #include <string>
 #include <unordered_map>
+#include <set>
 #include <vector>
+#include <assert.h>
 
 // Below are some suggestions on how you might want to divide up your project.
 // You may delete this and divide it up however you like.
 #include "ASTNode.hpp"
 #include "lexer.hpp"
 #include "SymbolTable.hpp"
+
+// Using
+using std::string;
+using std::endl;
+// Used to represent a collection of words (literal string)
+using words_t = std::set<std::string>;
+
+
+/** 
+*  Error function. (EG)
+*  Pass in an error message with any number of arguments
+*  Example use: Error(17, "Invalid value of x; x = ", x, ".");
+*/
+template <typename... Ts>
+void Error(size_t line_num, Ts... message) 
+{
+  std::cerr << "ERROR (Line " << line_num << "): ";
+  (std::cerr << ... << message);
+  std::cerr << endl;
+  // exit with non-zero 
+  exit(1);
+}
+
+/** 
+*  Error function with token. (EG)
+*  Pass in an error message with any number of arguments
+*  Example use: Error(tokens[token_id], "Invalid value of y; y = ", y, ".");
+*/
+template <typename... Ts>
+void Error(emplex::Token token, Ts... message) 
+{
+  Error(token.line_id, message...);
+}
 
 
 int main(int argc, char * argv[])
@@ -32,3 +67,4 @@ int main(int argc, char * argv[])
   // EXECUTE the AST to run your program.
   
 }
+
